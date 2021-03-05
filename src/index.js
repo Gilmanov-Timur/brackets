66fc7d176c824447b87637053b920c55ccb50393
tgilmanov <gtrediska@gmail.com>
2021-03-05 22:08:29 +0500

    feat: brackets task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const openBrackets = []
+	const closeBrackets = []
+	const brackets = []
+
+	bracketsConfig.forEach(bracket => {
+		openBrackets.push(bracket[0]);
+		closeBrackets.push(bracket[1]);
+	})
+
+	for (let i = 0; i < str.length; i++) {
+		const openIndex = openBrackets.indexOf(str[i])
+		const closeIndex = closeBrackets.indexOf(str[i])
+
+		if (openIndex !== -1 && closeIndex !== -1) {
+			if (brackets[brackets.length - 1] === str[i]) {
+				brackets.pop()
+			} else {
+				brackets.push(closeBrackets[openIndex])
+			}
+		} else if (openIndex !== -1) {
+			brackets.push(closeBrackets[openIndex])
+		} else if (closeIndex !== -1) {
+			if (brackets[brackets.length - 1] === str[i]) {
+				brackets.pop()
+			} else {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+
+	return brackets.length === 0
 }
